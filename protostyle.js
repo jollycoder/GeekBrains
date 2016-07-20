89ba303e6e1b0893cd779d02e11d85a4704eca28
jollycoder <dfiveg@gmail.com>
2016-07-20 13:59:40 +0300

    Update protostyle.js

--- protostyle.js
+++ protostyle.js
@@ -1,20 +1,21 @@
 function Table() {
-    this.rowsNumber = 1;
+    this.rowsNumber = 1;  // значения по умолчанию
     this.cellsNumber = 1;
 }
 
 Table.prototype.size = function (rowsNumber, cellsNumber) {
-    this.rowsNumber = rowsNumber || 1;
-    this.cellsNumber = cellsNumber || 1;
+    this.rowsNumber = rowsNumber;
+    this.cellsNumber = cellsNumber;
 };
 
 Table.prototype.createTable = function (func) {  // в параметре функция func для работы с создаваемыми ячейками
-    var table = document.createElement("table");
+    var table = document.createElement('table');
     for (var r = 0; r <= this.rowsNumber - 1; r++) {
         var row = table.insertRow();
         for (var c = 0; c <= this.cellsNumber - 1; c++) {
             var cell = row.insertCell();
-            func(r, c, cell);
+            if (typeof func === 'function')
+                func(r, c, cell);
         }
     }
     return table;
@@ -25,6 +26,7 @@ function ChessBoard(id)  {
     this._rowsNumber = 8;               // защищённые свойства, постоянные для шахматной доски
     this._cellsNumber = 8;
     this._letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+    this._cellsInfo = {};   // коллекция для записи информации о клетках
 }
 
 ChessBoard.prototype = Object.create(Table.prototype);
@@ -35,42 +37,65 @@ ChessBoard.prototype.initBoard = function (lightColor, darkColor, cellSize) {  /
     this.lightColor = lightColor || 'white';
     this.darkColor = darkColor || 'black';
     this.cellSize = cellSize || '30px';
+    this.outEventHandler = null;   // параметр для передачи внешней функции-обработчика событий
 
     var self = this;
-    var f = function (r, c, cell) {
+    function putBoardInfo(r, c, cell) {
+        var coord = self._letters[c] + (8 - r).toString();
+        cell.setAttribute('data-id', coord);  // устанавливаем каждой ячейке атрибут data-id с координатой вида A1
+        self._cellsInfo[coord] = {node: cell};
         if (r % 2 == 0)
             var color = (c % 2 == 0 ? self.lightColor : self.darkColor);
         else
             color = (c % 2 == 0 ? self.darkColor : self.lightColor);
         cell.style.backgroundColor = color;
+        self._cellsInfo[coord].color = (color == self.darkColor ? 'black' : 'white');
         cell.style.width = cell.style.height = self.cellSize;
-        cell.setAttribute( 'data-id', self._letters[c] + (8 - r).toString() );  // устанавливаем каждой ячейке атрибут data-id с координатой вида A1
-    };
-    this.table = this.createTable(f);
-    this.table.style.borderSpacing = "0";
+    }
+    this.table = this.createTable(putBoardInfo);
+    this.table.style.borderSpacing = '0';
     document.getElementById(this._id).appendChild(this.table);
-};
 
-ChessBoard.prototype.setActiveCell = function (cellNode)  {
-    (this.activeCell) && (this.activeCell.node.style.outline = "");  // снимаем выделение с предыдущей активной клетки, если была
-    cellNode.style.outline = "2px solid #808080";         // выделяем активную клетку
-    this.activeCell = {name: cellNode.getAttribute('data-id'), node: cellNode};
-    alert("Активная клетка: " + this.activeCell.name);
-};
-
-ChessBoard.prototype.setOnEvents = function (handler) {                   // подписываемся на события
-    var self = this;
-    ['click', 'dblclick', 'contextmenu', 'mouseover', 'mousedown', 'mouseup', 'mousemove'].forEach(function (item) {
-        self.table.addEventListener(item, handler);
+    ['click', 'dblclick', 'contextmenu', 'mouseover', 'mousedown', 'mouseup', 'mousemove'].forEach(function (item) {  // подписываемся на события
+        self.table.addEventListener(item, eventHandler);
     });
+    function eventHandler(event) {
+        switch (event.type) {
+            case 'dblclick':
+                alert('dblclick по клетке ' + self.getCoordFromNode(event.target));
+            // и т. д.
+        }
+        if (typeof self.outEventHandler === 'function')
+            self.outEventHandler(event);                // передаём события внешнему обработчику
+    }
 };
 
-myBoard = new ChessBoard("chessboardPlace");         // создаём доску в элементе с id "chessboardPlace"
-myBoard.initBoard("#ffce9e", "#d18b47", "35px");     // устанавливаем цвета, размеры клеток и координаты
-myBoard.setOnEvents(function eventHandler(event) {   // подписываемся на события
-    switch(event.type)  {
+ChessBoard.prototype.setActiveCell = function (coord)  {  // установка активной клетки по её координате
+    (this.activeCell) && (this.activeCell.node.style.outline = '');  // снимаем выделение с предыдущей активной клетки, если была
+    cellNode = this._cellsInfo[coord].node;
+    cellNode.style.outline = '2px solid #808080';         // выделяем активную клетку
+    this.activeCell = {name: coord, node: cellNode};
+};
+
+ChessBoard.prototype.getCoordFromNode = function (node) {   // получить координату по Node клетки
+    for (var key in this._cellsInfo)  {
+        if (this._cellsInfo[key].node === node) {
+            return key;
+        }
+    }
+};
+
+ChessBoard.prototype.setOutEventHandler = function (handler) {
+    this.outEventHandler = handler;
+};
+
+myBoard = new ChessBoard('chessboardPlace');         // создаём доску в элементе с id 'chessboardPlace'
+myBoard.initBoard('#ffce9e', '#d18b47', '35px');     // устанавливаем цвета, размеры клеток и координаты вида A1
+myBoard.setOutEventHandler(function (event) {        // задаём функцию для внешней обработки событий
+    switch (event.type) {
         case 'click':
-            myBoard.setActiveCell(event.target);
+            var coord = myBoard.getCoordFromNode(event.target);  // получаем координату вида A1
+            myBoard.setActiveCell(coord);                        // устанавливаем активную клетку по координате
         // и т. д.
     }
 });
