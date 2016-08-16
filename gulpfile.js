85a3618bba2d6d2f2382198addbdfa2e1578c5e5
jollycoder <dfiveg@gmail.com>
2016-08-16 19:11:55 +0300

    Create gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp');
+var babel = require('gulp-babel');
+
+gulp.task('transpile', function () {
+    return gulp.src('es2015/*.js')
+        .pipe(babel({ presets: ['es2015']} ))
+        .pipe(gulp.dest('es5'));
+});
