94faa269a6dafa517f5a7de895eb0eaa87b35027
jollycoder <dfiveg@gmail.com>
2016-08-16 19:38:58 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,6 @@ var babel = require('gulp-babel');
 
 gulp.task('transpile', function () {
     return gulp.src('es2015/*.js')
-        .pipe(babel({ presets: ['es2015'] }))
-        .pipe(gulp.dest('es5'));
+        .pipe( babel({ presets: ['es2015'] }) )
+        .pipe( gulp.dest('es5') );
 });
